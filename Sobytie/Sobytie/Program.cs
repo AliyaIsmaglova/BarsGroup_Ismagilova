28eb572f602a4c8796e2cbde79c270ba32fdf416
Aliya Ismagilova <75615381+AliyaIsmaglova@users.noreply.github.com>
2022-03-31 20:38:54 +0300

    Add files via upload

---
+++ Sobytie/Sobytie/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobytie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sob sob = new Sob();
+
+            sob.OnKeyPressed += Sob_OnKeyPressed;
+            Console.WriteLine("Для завершения нажмите C");
+            Console.WriteLine("Введите символ");
+
+            sob.Run();
+        }
+
+        static void Sob_OnKeyPressed(Object sender, char sobyt)
+        {
+            Console.WriteLine($"{sobyt}");
+        }
+    }
+
+    class Sob
+    {
+        public event EventHandler<char>? OnKeyPressed;
+
+        ConsoleKeyInfo key;
+
+        public void Run()
+        {
+            while (Console.ReadKey().Key != ConsoleKey.C)
+            {
+
+                key = Console.ReadKey();
+                char sobyt = key.KeyChar;
+
+                OnKeyPressed?.Invoke(this, sobyt);
+            }
+
+        }
+    }
+}
